d3462d7d5aaa3b648d9bbd4d62df542b1179ad32
abbeyroyse13 <heatherruan126@gmail.com>
2021-09-21 16:00:30 -0500

    working on owners

---
+++ Repositories/OwnerRepository.cs
@@ -0,0 +1,64 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace DogGo.Repositories
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private readonly IConfiguration _config; 
+
+        public OwnerRepository(IConfiguration config)
+        {
+            _config = config; 
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+        //fix this soon 
+        public List<Owner> GetAllOwners()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, Email, [Name], Address, NeighborhoodId, Phone
+                        FROM Owner
+                    ";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Owner> owners = new List<Owner>();
+                    while (reader.Read())
+                    {
+                        Owner owner = new Owner
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Email = reader.GetString(reader.GetOrdinal("Email")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Address = reader.GetString(reader.GetOrdinal("Address")),
+                            NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId")),
+                            Phone = reader.GetInt32(reader.GetOrdinal("Phone"))
+                        };
+
+                        owners.Add(owner);
+                    }
+
+                    reader.Close();
+
+                    return owners;
+                }
+            }
+        }
+
+
+    }
+}
